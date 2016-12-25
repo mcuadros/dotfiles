981c764af3ba71926dcdb7ee3bfc07615d92bfe9
Máximo Cuadros <mcuadros@gmail.com>
2016-12-25 19:38:28 +0100

    cleanup fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# requires 'powerline' arch package
 set fish_function_path $fish_function_path "/usr/lib/python2.7/site-packages/powerline/bindings/fish"
 powerline-setup
 
@@ -7,7 +8,7 @@ set -gx CLOUDSDK_PYTHON /usr/bin/python2
 set -gx EDITOR /usr/bin/vim
 set -gx BROWSER /usr/bin/google-chrome-stable
 
-
+# requires https://github.com/tuvistavie/fundle 
 fundle plugin 'edc/bass'
 fundle plugin 'tuvistavie/fish-fastdir'
 fundle plugin 'tuvistavie/fish-ssh-agent'
@@ -20,6 +21,4 @@ bass source '/opt/google-cloud-sdk/completion.bash.inc'
 alias ical='gcalcli --calendar maximo@sourced.tech agenda'           
 alias lock='.config/i3/lock.sh'
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
-#xrandr --output eDP1 --dpi 200 --auto --output DP1 --dpi 96 --auto --rotate right --right-of eDP1
 
