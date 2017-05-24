bfc1f52eeaa2b0439a474fdb4d62e60b1bd8ce60
Máximo Cuadros <mcuadros@gmail.com>
2017-05-24 10:28:05 +0200

    minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
-# requires 'powerline' arch package
-set fish_function_path $fish_function_path "/usr/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
-
 set -gx GOPATH "/home/mcuadros/workspace/go"
 set -gx PATH $GOPATH/bin/ $PATH
 set -gx CLOUDSDK_PYTHON /usr/bin/python2
@@ -13,15 +9,14 @@ set -gx PATH $HOME/.cargo/bin $PATH
 
 # requires https://github.com/tuvistavie/fundle
 fundle plugin 'edc/bass'
-fundle plugin 'tuvistavie/fish-fastdir'
 fundle plugin 'tuvistavie/fish-ssh-agent'
 fundle init
 
 bass source '/opt/google-cloud-sdk/path.bash.inc'
 bass source '/opt/google-cloud-sdk/completion.bash.inc'
 
-
 alias ical='gcalcli --calendar maximo@sourced.tech agenda'
 alias lock='.config/i3/lock.sh'
-alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias battle='wine "./.wine/drive_c/Program Files (x86)/Blizzard App/Battle.net.8733/Battle.net.exe"'
 
+alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
