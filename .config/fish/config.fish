7ddd6cf2aceb497b1a3f079f1699d5cc429f90a8
Máximo Cuadros <mcuadros@gmail.com>
2018-02-20 00:03:31 +0100

    fish: config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,9 @@ set -gx PATH $GOPATH/bin/ $PATH
 set -gx CLOUDSDK_PYTHON /usr/bin/python2
 set -gx EDITOR /usr/bin/vim
 set -gx BROWSER /usr/bin/google-chrome-stable
-
-
-set -gx PATH $HOME/.cargo/bin $PATH
+set -gx QT_AUTO_SCREEN_SCALE_FACTOR 1.5 
+set -gx PATH $HOME/.config/yarn/global/node_modules/.bin/  $PATH
+set -x GPG_TTY (tty)
 
 # requires https://github.com/tuvistavie/fundle
 fundle plugin 'edc/bass'
@@ -20,3 +20,7 @@ alias lock='.config/i3/lock.sh'
 alias battle='wine "./.wine/drive_c/Program Files (x86)/Blizzard App/Battle.net.8733/Battle.net.exe"'
 
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+set -gx ONLINE_TOKEN d9130bbca4b37d58aa8767fa78ddf46fa926c8ad
+set -gx OCTOPRINT_HOST http://10.3.167.147/
+set -gx OCTOPRINT_APIKEY DE3E488D96C44AB182FF8608223784F9   
